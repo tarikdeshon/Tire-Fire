f7c19973e69d42fd7f215d177899b748d14b1357
unknown <46696435+illbeheer@users.noreply.github.com>
2019-05-03 21:21:41 -0700

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
+// testttttttttttt
 
 
-    
     // get trackers
-    var userinput= "";
-    var weedname= "";
-    var weedrace= "";
-    var weedeffect= "";
-    var weedflavor= "";
-    
-    
-   //search funtionality
-    $("#search2").on("click", function(event1){
+    var userinput = "";
+    var weedname = "";
+    var weedrace = "";
+    var weedeffect = "";
+    var weedflavor = "";
+
+
+    //search funtionality
+    $("#search2").on("click", function (event1) {
         event1.preventDefault()
         userinput = $("#search").val()
-        
-    //use the search for api
-        var queryURL = "http://strainapi.evanbusse.com/tHgjB63/strains/search/name/"+ userinput;
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-        console.log(response)
-         
     
-    //response for search results
-          
-        //actually get a response
-         
-        //append results strain name etc.
+        //use the search for api
+        var queryURL = "http://strainapi.evanbusse.com/tHgjB63/strains/search/name/" + userinput;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            $("#weedname").append(response)
+
+            console.log(response)
+            
 
 
-    })
-        
-    
+            //response for search results
 
-    
+            //actually get a response
 
-
+            //append results strain name etc.
 
 
+        })
 
 
 
-   });   
+
+
+
+
+
+
+
+
+    });
 
 
 
