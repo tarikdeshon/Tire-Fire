8435ae9ab67c7eb32ab29735824aa6a84c55b16f
Tarik P <tarikdeshon@gmail.com>
2019-05-03 23:43:02 -0700

    slideshow setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
 
 
-    
+
     // get trackers
-    var userinput= "";
-    var weedname= "";
-    var weedrace= "";
-    var weedeffect= "";
-    var weedflavor= "";
-    
-    
-   //search funtionality
-    $("#search2").on("click", function(event1){
+    var userinput = "";
+    var weedname = "";
+    var weedrace = "";
+    var weedeffect = "";
+    var weedflavor = "";
+
+
+    //search funtionality
+    $("#search2").on("click", function (event1) {
         event1.preventDefault()
         userinput = $("#search").val()
-        
-    //use the search for api
-        var queryURL = "http://strainapi.evanbusse.com/tHgjB63/strains/search/name/"+ userinput;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+        //use the search for api
+        var queryURL = "http://strainapi.evanbusse.com/tHgjB63/strains/search/name/" + userinput;
 
-        console.log(response)
-         
-    
-    //response for search results
-          
-        //actually get a response
-         
-        //append results strain name etc.
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            console.log(response)
 
 
-    })
-        
-    
+            //response for search results
 
-    
+            //actually get a response
+
+            //append results strain name etc.
+
+
+        })
 
 
 
@@ -47,7 +43,11 @@ $(document).ready(function() {
 
 
 
-   });   
+
+
+
+
+    });
 
 
 
